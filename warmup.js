0518a1ca13fb3ad598fdb2dfa8a3f945a3196413
emilyzimmerman <zimmermanemily121@gmail.com>
2022-10-03 18:00:10 -0500

    Initial commit

---
+++ warmup.js
@@ -0,0 +1,22 @@
+// let starCount = 5; 
+
+// let string =''; 
+
+// for(let i =0; i<starCount; i++){
+//     //printing space
+//     for(let j = 0; j<starCount; j++){
+//         string += " "; 
+//     }
+//     //printing star
+//     for(let k = 0; k<i; k++){
+//         string += "*"; 
+//     }
+// }
+
+// console.log(string);
+
+//Correct Answer 
+
+for(let i = 1; i<6; i++){
+    console.log('*'.repeat(i)); 
+}
